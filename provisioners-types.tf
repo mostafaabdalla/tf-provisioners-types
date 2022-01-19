e1d40a119fcf35b54dee4503571c1d73f2003ba5
mostafa <mostafaa.abdallaa@gmail.com>
2022-01-19 18:05:59 +0000

    removed the egress rule to make the creation provisioner fails

--- provisioners-types.tf
+++ provisioners-types.tf
@@ -14,13 +14,14 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
+  /*  egress {
     description = "Outbound Allowed"
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  */
 }
 
 
